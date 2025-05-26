e1df08d138fc22aaa4c3c2167e180d8b7592cb5b
Your Name <you@example.com>
2025-05-26 16:07:57 -0700

    update 3

--- tests.cpp
+++ tests.cpp
@@ -40,4 +40,42 @@ public:
         vertices[u]->edges.emplace_back(u, v, w);
         vertices[v]->edges.emplace_back(v, u, w); 
     }
+
+
+vector<Edge> findMST(){
+    int n = vertices.size();
+    vector<bool> inMST(n, false); // Track vertices included in the MST
+    vector<double> key(n, numeric_limits<double>::max()); // Key values to pick minimum weight edge
+    vector<int> parent(n, -1); // Parent array to store the MST structure
+    key[0] = 0;
+
+    using pii = pair<double, int>; // Pair to store key and vertex index
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0,0});
+
+    while(!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (inMST[u]) continue;
+        inMST[u] = true;
+
+        for (auo& e : vertices[u]->edges) {
+            int v = e.destination;
+            double w = e.weight;
+            if (!inMST[v] && w < key[v]) {
+                key[v] = w;
+                parent[v] = u;
+                pq.push(key[v]. v);
+            }
+        }
+    }
+
+    vector<Edge> mst;
+    for (int v = 1; v < n; ++v) {
+        if (parent[v] != -1) 
+            mst.emplace_back(parent[v], v, key[v]);
+    }
+    return mst;
 }
+};
